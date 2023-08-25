fe77f77bd2901e916cf3e63eda394272e1aec5ee
Frank LSY <429598469@qq.com>
2023-08-25 12:28:40 +0800

    放弃瀑布，改用简单折线

--- front/src/components/ResultDialog.vue
+++ front/src/components/ResultDialog.vue
@@ -10,7 +10,7 @@
           <span class="text-gray-100"
             >{{ store.getters.getQuestions[0]["title"] }}:
           </span>
-          <span class="text-amber-400">{{
+          <span class="text-amber-300">{{
             store.getters.getQuestions[0]["choice"][
               store.getters.getAnswers[
                 store.getters.getQuestions[0]["questionid"]
@@ -26,7 +26,7 @@
           <span class="text-gray-100"
             >{{ store.getters.getQuestions[num]["title"] }}:
           </span>
-          <span class="text-amber-400">{{
+          <span class="text-amber-300">{{
             store.getters.getQuestions[num]["choice"][
               store.getters.getAnswers[
                 store.getters.getQuestions[num]["questionid"]
@@ -36,11 +36,11 @@
         </div>
         <div class="w-1/2 sm:w-1/4 text-left pl-2 my-0.5">
           <span class="text-gray-100">吸烟情况: </span>
-          <span class="text-amber-400">{{ smokeC }}</span>
+          <span class="text-amber-300">{{ smokeC }}</span>
         </div>
         <div class="w-1/2 sm:w-1/4 text-left pl-2 my-0.5">
           <span class="text-gray-100">癌症史: </span>
-          <span class="text-amber-400">{{
+          <span class="text-amber-300">{{
             store.getters.getQuestions[3]["choice"][
               store.getters.getAnswers[
                 store.getters.getQuestions[3]["questionid"]
@@ -50,7 +50,7 @@
         </div>
         <div class="w-1/2 sm:w-1/4 text-left pl-2 my-0.5">
           <span class="text-gray-100">家族史: </span>
-          <span class="text-amber-400">{{
+          <span class="text-amber-300">{{
             store.getters.getQuestions[4]["choice"][
               store.getters.getAnswers[
                 store.getters.getQuestions[4]["questionid"]
@@ -66,7 +66,7 @@
           <span class="text-gray-100"
             >{{ ceDict[store.getters.getQuestions[num]["questionid"]] }}:
           </span>
-          <span class="text-amber-400">{{
+          <span class="text-amber-300">{{
             store.getters.getQuestions[num]["choice"][
               store.getters.getAnswers[
                 store.getters.getQuestions[num]["questionid"]
@@ -75,9 +75,9 @@
           }}</span>
         </div>
       </div>
-      <div>
+      <!-- <div>
         {{ prob_dict }}
-      </div>
+      </div> -->
       <div id="riskChart" class="absolute bottom-2 w-full h-trentecinq"></div>
     </div>
   </div>
@@ -109,16 +109,30 @@ const prob = ref([]);
 const prob_dict = ref({});
 var option = {
   title: {
-    text: "各因素概率",
+    text: "各因素致肺癌预测概率贡献",
+    textStyle: {
+      color: "#f3f4f6",
+    },
   },
   tooltip: {
     trigger: "axis",
     axisPointer: {
-      type: "shadow",
+      type: "cross",
     },
     formatter: function (params) {
-      var tar = params[1];
-      return tar.name + " : " + tar.value;
+      var line = params[0];
+      var assis = params[1];
+      //   console.log(line[0])
+      return (
+        "累计肺癌风险值: " +
+        line.value +
+        "% <br/> <b>" +
+        line.name +
+        "</b> 单因素肺癌风险增值: " +
+        " : " +
+        assis.value +
+        "%"
+      );
     },
   },
   grid: {
@@ -129,34 +143,76 @@ var option = {
   },
   xAxis: {
     type: "category",
+    boundaryGap: false,
     splitLine: { show: false },
+
+    axisLabel: {
+      interval: 0,
+      rotate: 30,
+      color: "#f3f4f6",
+      fontWeight: "bold",
+    },
   },
   yAxis: {
     type: "value",
+    min: 0,
+    axisLabel: {
+      interval: 0,
+      color: "#f3f4f6",
+      fontWeight: "bold",
+    },
   },
   series: [
+    // {
+    //   name: "",
+    //   type: "bar",
+    //   stack: "Total",
+    //   itemStyle: {
+    //     borderColor: "transparent",
+    //     color: "transparent",
+    //   },
+    //   emphasis: {
+    //     itemStyle: {
+    //       borderColor: "transparent",
+    //       color: "transparent",
+    //     },
+    //   },
+    // },
+    // {
+    //   name: "",
+    //   type: "bar",
+    //   stack: "Total",
+    //   label: {
+    //     show: true,
+    //     position: "inside",
+    //   },
+    // },
     {
-      name: "Placeholder",
-      type: "bar",
+      name: "",
+      type: "line",
       stack: "Total",
-      itemStyle: {
-        borderColor: "transparent",
-        color: "transparent",
-      },
-      emphasis: {
-        itemStyle: {
-          borderColor: "transparent",
-          color: "transparent",
+      areaStyle: {},
+      label: {
+        show: true,
+        position: "top",
+        distance: 10,
+        rotate: 30,
+        textStyle: {
+          color: "#fcd34d",
+          fontWeight: "bold",
+        },
+        formatter: function (params) {
+          // console.log(params)
+          return params.value + "%";
         },
       },
     },
     {
-      name: "Life Cost",
+      name: "",
       type: "bar",
-      stack: "Total",
-      label: {
-        show: true,
-        position: "inside",
+      itemStyle: {
+        borderColor: "transparent",
+        color: "transparent",
       },
     },
   ],
@@ -169,22 +225,25 @@ const calScore = () => {
     prob_dict.value = store.getters.getProb;
     smokeC.value = store.getters.getSmoke;
 
-    option.xAxis.data = Object.keys(prob_dict.value);
-    prob.value = Object.values(prob_dict.value);
-    prob.value.pop();
-    prob.value.unshift(0);
-    option.series[0].data = prob.value;
-    option.series[1].data = [];
-    for (let i = 0; i < prob.value.length; i++) {
-      option.series[1].data.push(
-        (Object.values(prob_dict.value)[i] - prob.value[i]).toFixed(2)
-      );
+    var keys = Object.keys(prob_dict.value);
+    option.xAxis.data = [];
+    keys.forEach((item) => {
+      option.xAxis.data.push(ceDict[item]);
+    });
+
+    // prob.value.pop(0);
+    // prob.value.unshift(0);
+    var prob1 = Object.values(prob_dict.value);
+    var prob2 = [prob1[0]];
+    for (var i = 1; i < prob1.length; i++) {
+      prob2.push((prob1[i] - prob1[i - 1]).toFixed(2));
     }
+    option.series[0].data = prob1;
+    option.series[1].data = prob2;
 
     chartDom = document.getElementById("riskChart");
     myChart = echarts.init(chartDom);
     option && myChart.setOption(option);
-
   } else {
     resultsAPI
       .calcProbability({
@@ -192,20 +251,21 @@ const calScore = () => {
         answers: store.getters.getAnswers,
       })
       .then((res) => {
-        console.log(res.data.probability);
+        // console.log(res.data.probability);
         qid.value = Object.keys(res.data.probability);
         prob_dict.value = res.data.probability;
-        option.xAxis.data = Object.keys(res.data.probability);
-        prob.value = Object.values(prob_dict.value);
-        prob.value.pop();
-        prob.value.unshift(0);
-        option.series[0].data = prob.value;
-        option.series[1].data = [];
-        for (let i = 0; i < prob.value.length; i++) {
-        option.series[1].data.push(
-            (Object.values(prob_dict.value)[i] - prob.value[i]).toFixed(2)
-        );
+        var keys = Object.keys(prob_dict.value);
+        option.xAxis.data = [];
+        keys.forEach((item) => {
+          option.xAxis.data.push(ceDict[item]);
+        });
+        var prob1 = Object.values(prob_dict.value);
+        var prob2 = [prob1[0]];
+        for (var i = 1; i < prob1.length; i++) {
+          prob2.push((prob1[i] - prob1[i - 1]).toFixed(2));
         }
+        option.series[0].data = prob1;
+        option.series[1].data = prob2;
 
         chartDom = document.getElementById("riskChart");
         myChart = echarts.init(chartDom);
@@ -251,7 +311,7 @@ const colorPercent = () => {
 onMounted(() => {
   calScore();
 
-  console.log(Object.keys(store.getters.getAnswers));
+  //   console.log(Object.keys(store.getters.getAnswers));
 });
 
 const ceDict = {
@@ -263,10 +323,10 @@ const ceDict = {
   smoking: "吸烟状态",
   packYear: "年吸烟量",
   smokingIntensity: "每天吸烟",
-  MMEF: "MMEF ml/sec",
-  FEV1: "FEV1 %",
-  AFP: "AFP ng/ml",
-  CEA: "CEA ng/ml",
-  CRP: "CRP mg/L",
+  MMEF: "MMEF (ml/sec)",
+  FEV1: "FEV1 (%)",
+  AFP: "AFP (ng/ml)",
+  CEA: "CEA (ng/ml)",
+  CRP: "CRP (mg/L)",
 };
 </script>
